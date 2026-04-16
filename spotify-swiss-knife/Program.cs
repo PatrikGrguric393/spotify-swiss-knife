6da789f2dd4b8d4241f71ae4ccddb260a7afb418
Patrik Grgurić <patrik.grguric@grghomelab.me>
2026-04-16 17:45:58 +0200

    CSS, routes

--- spotify-swiss-knife/Program.cs
+++ spotify-swiss-knife/Program.cs
@@ -49,6 +49,7 @@ builder.Services.AddSingleton<TrackMockRepository>();
 builder.Services.AddSingleton<AlbumMockRepository>();
 builder.Services.AddSingleton<ArtistMockRepository>();
 builder.Services.AddSingleton<PlaylistMockRepository>();
+builder.Services.AddSingleton<ShuffleSchedulerService>();
 
 var app = builder.Build();
 
@@ -66,31 +67,6 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "library-songs",
-    pattern: "library/songs",
-    defaults: new { controller = "Library", action = "Songs" });
-
-app.MapControllerRoute(
-    name: "library-albums",
-    pattern: "library/albums",
-    defaults: new { controller = "Library", action = "Albums" });
-
-app.MapControllerRoute(
-    name: "library-artists",
-    pattern: "library/artists",
-    defaults: new { controller = "Library", action = "Artists" });
-
-app.MapControllerRoute(
-    name: "library-playlists",
-    pattern: "library/playlists",
-    defaults: new { controller = "Library", action = "Playlists" });
-
-app.MapControllerRoute(
-    name: "library",
-    pattern: "library",
-    defaults: new { controller = "Library", action = "Index" });
 
 app.MapControllerRoute(
     name: "default",
